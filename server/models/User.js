8d6356e290abfa1d370c2aacbc0a81475c2a2814
Your midhunkumar <your-umamidhunkumar2005@gmail.com>
2026-04-14 14:23:58 +0530

    Saving User model and fixing findOne error

---
+++ server/models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  xp: { type: Number, default: 0 },
+  level: { type: Number, default: 1 }
+});
+
+module.exports = mongoose.model('User', userSchema);
